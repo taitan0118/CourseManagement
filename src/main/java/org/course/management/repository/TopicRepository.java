99396cf99455eda8211126f0ab6503be65435417
tramy1511 <nguyentramy151124@gmail.com>
2024-06-20 11:17:04 +0700

    crud topic

---
+++ src/main/java/org/course/management/repository/TopicRepository.java
@@ -0,0 +1,1 @@
+package org.course.management.repository;import org.course.management.entity.Topic;import org.springframework.data.jpa.repository.JpaRepository;import org.springframework.data.jpa.repository.Query;import org.springframework.stereotype.Repository;@Repositorypublic interface TopicRepository extends JpaRepository<Topic, Integer> {}
